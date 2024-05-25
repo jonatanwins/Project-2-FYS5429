5102f1f67ce8086c898b4d153e4c32d48f7e1f46
Daniel Steeneveldt <daniel.steeneveldt@gmail.com>
2024-05-25 11:23:31 +0200

    dummy data bug

--- project_2/src/lorenz/dummy_data.py
+++ project_2/src/lorenz/dummy_data.py
@@ -1,16 +1,25 @@
-import torch
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
 
 class MyDataset(Dataset):
     def __init__(self, num_samples, num_features):
-        self.data1 = torch.randn(num_samples, num_features)
-        self.data2 = torch.randint(0, 2, (num_samples,))
+        self.data1 = np.random.randn(num_samples, num_features).astype(np.float32)
+        self.data2 = np.random.randint(0, 2, num_samples).astype(np.int64)
 
     def __len__(self):
         return len(self.data1)
 
     def __getitem__(self, idx):
         return self.data1[idx], self.data2[idx]
+
+def numpy_collate(batch):
+    if isinstance(batch[0], np.ndarray):
+        return np.stack(batch)
+    elif isinstance(batch[0], (tuple, list)):
+        transposed = zip(*batch)
+        return [numpy_collate(samples) for samples in transposed]
+    else:
+        return np.array(batch)
 
 def get_dummy_dataloader(num_datapoints, num_features, batch_size, shuffle=True, num_workers=0, persistent_workers=False):
     # Create the dataset
@@ -22,7 +31,8 @@ def get_dummy_dataloader(num_datapoints, num_features, batch_size, shuffle=True,
         batch_size=batch_size, 
         shuffle=shuffle, 
         num_workers=num_workers, 
-        persistent_workers=persistent_workers
+        persistent_workers=persistent_workers,
+        collate_fn=numpy_collate
     )
     
     return dataloader
@@ -41,5 +51,6 @@ if __name__ == "__main__":
     for batch in dataloader:
         x, y = batch
         print(f'x: {x.shape}, y: {y.shape}')
-        # Do something with x and y
-        break  # Just to demonstrate, remove this line for full iteration
+        # Convert torch tensors to numpy arrays for compatibility with Flax/JAX
+        x, y = np.array(x), np.array(y)
+        print(f'Converted x: {x.shape}, y: {y.shape}')
