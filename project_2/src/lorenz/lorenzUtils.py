46781ac70e2d45aa51b3348bc144cba4d336fe14
Daniel Steeneveldt <daniel.steeneveldt@gmail.com>
2024-06-10 01:17:44 +0200

    simplefied lorenzUtils.py

--- project_2/src/lorenz/lorenzUtils.py
+++ project_2/src/lorenz/lorenzUtils.py
@@ -1,18 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import sys
 sys.path.append('..')
-from sindyLibrary import sindy_library_factory
 from sindySimulate import sindy_simulate
-from lorenzData import generate_lorenz_data
-from dev.loss_old import loss_dynamics_x_factory, loss_dynamics_z_factory, recon_loss_factory
 from jax import jit
 import jax.numpy as jnp
 
-def coordinate_transformation(xi, test_data):
+def generate_initial_conditions(n_ics, ic_means=np.array([0, 0, 25]), ic_widths=2 * np.array([36, 48, 41])):
+    """
+    Generate initial conditions for training sample.
+    """
+    return ic_widths * (np.random.rand(n_ics, 3) - 0.5) + ic_means
+
+def coordinate_transformation(xi, test_ic, sindy_coefficients):
     a1 = 1
-    a2 = test_data['sindy_coefficients'][2, 0] / xi[2, 0]
+    a2 = sindy_coefficients[2, 0] / xi[2, 0]
     a3 = np.sqrt(-xi[5, 2] / xi[6, 1] * a2 ** 2)
     b3 = -xi[0, 2] / xi[3, 2]
 
@@ -25,16 +27,16 @@ def coordinate_transformation(xi, test_data):
     sindy_coefficients_transformed[3, 2] = xi[3, 2]
     sindy_coefficients_transformed[5, 2] = xi[5, 2] * a1 * a2 / a3
 
-    z0_transformed = np.array([test_data['z'][0, 0] / a1, test_data['z'][0, 1] / a2, (test_data['z'][0, 2] - b3) / a3])
+    z0_transformed = np.array([test_ic[0, 0] / a1, test_ic[0, 1] / a2, (test_ic[0, 2] - b3) / a3])
 
     return z0_transformed, sindy_coefficients_transformed
 
-def simulate_ode(test_data, t, xi, xi_transformed=None, params=None):
+def simulate_ode(test_ic, t, xi, xi_transformed=None, params=None):
     lib_kwargs = {"poly_order": params['poly_order'], "include_sine": params['include_sine'], "n_states": params['input_dim']}
-    z_sim = sindy_simulate(test_data['z'][0], t, xi, **lib_kwargs)
+    z_sim = sindy_simulate(test_ic, t, xi, **lib_kwargs)
 
     if xi_transformed is not None:
-        z0_transformed, sindy_coefficients_transformed = coordinate_transformation(xi_transformed, test_data)
+        z0_transformed, sindy_coefficients_transformed = coordinate_transformation(xi_transformed, test_ic, xi)
         z_sim_transformed = sindy_simulate(z0_transformed, t, sindy_coefficients_transformed, **lib_kwargs)
         return z_sim, z_sim_transformed
 
@@ -63,7 +65,7 @@ def plot_sindy_coefficients(xi, xi_transformed=None, xi_true=None):
 
     plt.show()
 
-def plot_single_trajectory(t, z_sim, z_sim_transformed=None, test_data=None):
+def plot_single_trajectory(t, z_sim, z_sim_transformed=None, true_z=None):
     fig = plt.figure(figsize=(9, 3) if z_sim_transformed is not None else (3, 3))
     
     ax1 = fig.add_subplot(1, 3, 1, projection='3d')
@@ -77,15 +79,15 @@ def plot_single_trajectory(t, z_sim, z_sim_transformed=None, test_data=None):
         ax2.set_title('Transformed Dynamics')
         plt.axis('off')
     
-    if test_data is not None:
+    if true_z is not None:
         ax3 = fig.add_subplot(1, 3, 3, projection='3d')
-        ax3.plot(test_data['z'][:, 0], test_data['z'][:, 1], test_data['z'][:, 2], linewidth=2)
+        ax3.plot(true_z[:, 0], true_z[:, 1], true_z[:, 2], linewidth=2)
         ax3.set_title('True Dynamics')
         plt.axis('off')
     
     plt.show()
 
-def create_out_of_distribution_initial_conditions(n_ics, inDist_ic_widths, outDist_extra_width, t, params, noise_strength=1e-6):
+def create_out_of_distribution_initial_conditions(n_ics, inDist_ic_widths, outDist_extra_width, noise_strength=1e-6):
     full_width = inDist_ic_widths + outDist_extra_width
     ics = np.zeros((n_ics, 3))
     i = 0
@@ -103,13 +105,10 @@ def create_out_of_distribution_initial_conditions(n_ics, inDist_ic_widths, outDi
             ics[i] = ic
             i += 1
     
-    test_data = generate_lorenz_data(ics, t, params['input_dim'], linear=False, normalization=np.array([1/40, 1/40, 1/40]))
-    test_data['x'] = test_data['x'].reshape((-1, params['input_dim']))
-    test_data['x'] += noise_strength * np.random.normal(size=test_data['x'].shape)
-    test_data['dx'] = test_data['dx'].reshape((-1, params['input_dim']))
-    test_data['dx'] += noise_strength * np.random.normal(size=test_data['dx'].shape)
+    # Add noise to the initial conditions
+    noisy_ics = ics + noise_strength * np.random.normal(size=ics.shape)
     
-    return test_data
+    return noisy_ics
 
 
 if __name__ == "__main__":
@@ -125,22 +124,19 @@ if __name__ == "__main__":
     with open(f"{checkpoint_path}/hparams.json", "r") as f:
         hparams = json.load(f)
 
-    # Get sindy coefficients and mask from the trained model
+    # Get SINDy coefficients and mask from the trained model
     sindy_coefficients = trainer.state.params["sindy_coefficients"]
     mask = trainer.state.mask
 
     # Generate minimal test data
-    t = np.arange(0, 1, 0.01)  # minimal time span
+    t = np.arange(0, 1, 0.1)  # minimal time span
     z0 = np.array([[-8, 7, 27]])  # single initial condition
-    test_data = generate_lorenz_data(z0, t, hparams["input_dim"], linear=False, normalization=np.array([1/40, 1/40, 1/40]))
-    test_data['x'] = test_data['x'].reshape((-1, hparams['input_dim']))
-    test_data['dx'] = test_data['dx'].reshape((-1, hparams['input_dim']))
 
     # Simulate ODE with the discovered coefficients
-    z_sim = simulate_ode(test_data, t, sindy_coefficients, params=hparams)
+    z_sim = simulate_ode(z0, t, sindy_coefficients, params=hparams)
 
-    # Plot sindy coefficients
+    # Plot SINDy coefficients
     plot_sindy_coefficients(sindy_coefficients)
 
     # Plot the single trajectory
-    plot_single_trajectory(t, z_sim, test_data=test_data)
+    plot_single_trajectory(t, z_sim)
