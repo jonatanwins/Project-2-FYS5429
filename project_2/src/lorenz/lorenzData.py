c377e5ab347bfc75059e3ebaa16d996aff6c14bb
Daniel Steeneveldt <daniel.steeneveldt@gmail.com>
2024-06-12 00:25:41 +0200

    testing lorenzUtils on ml nodes

--- project_2/src/lorenz/lorenzData.py
+++ project_2/src/lorenz/lorenzData.py
@@ -178,7 +178,7 @@ def get_lorenz_data(n_ics, noise_strength=0, test_data=False):
     # Generate initial conditions
     ics = ic_widths * (np.random.rand(n_ics, 3) - 0.5) + ic_means
 
-    data = generate_lorenz_data(ics, t, input_dim, noise_strength)
+    data = generate_lorenz_data(ics, t, input_dim, noise_strength, normalization=np.array([1 / 40, 1 / 40, 1 / 40]))
 
     x = data["x"].reshape((-1, input_dim)) + noise_strength * np.random.randn(n_steps * n_ics, input_dim)
     dx = data["dx"].reshape((-1, input_dim)) + noise_strength * np.random.randn(n_steps * n_ics, input_dim)
--- project_2/src/lorenz/lorenzTraining.py
+++ project_2/src/lorenz/lorenzTraining.py
@@ -3,7 +3,8 @@ import sys
 from jax.lib import xla_bridge
 
 sys.path.append('../')
-from lorenzData import get_lorenz_data
+from lorenzUtils import get_lorenz_data
+
 from data_utils import create_jax_batches_factory
 from trainer import SINDy_trainer
 
